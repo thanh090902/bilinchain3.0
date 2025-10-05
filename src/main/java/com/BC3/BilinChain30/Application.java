771f2cdcf6cf8824dca05864b60489866011e86d
thanh090902 <thanhthaiit92@gmail.com>
2025-10-05 15:34:25 +0700

    first authen commit

---
+++ src/main/java/com/BC3/BilinChain30/Application.java
@@ -0,0 +1,13 @@
+package com.BC3.BilinChain30;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/BC3/BilinChain30/controller/AuthController.java
@@ -0,0 +1,5 @@
+package com.BC3.BilinChain30.controller;
+
+public class AuthController {
+
+}
